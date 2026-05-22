c7cf8913d45ce62ae37dec0ffebc74dad09dad94
Justin Ju <jj1824@ic.ac.uk>
2026-05-22 14:14:36 +0100

    Delete data/preview.py

--- data/preview.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_parquet('/Users/JustinJu/Desktop/Quant_Infrastructure/Quant_trading_infrastructure/data/BTC_USDT_1h.parquet')
-print(f"this is number of rows: {len(df)}")
-print(df.tail())
-print(df.head())
